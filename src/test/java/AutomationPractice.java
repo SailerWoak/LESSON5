834c56bd71425da3be80cea7234cda6159e9d32f
artur <artur.gabetov@gmail.com>
2018-11-08 14:05:03 +0200

    HomeWork 5

--- src/test/java/AutomationPractice.java
+++ src/test/java/AutomationPractice.java
@@ -13,9 +13,9 @@ import java.util.List;
 
 public class AutomationPractice {
     private final String HOME_PAGE = "http://automationpractice.com/index.php";
-    private final By ARTICLES = By.xpath(".//a[@class = 'sf-with-ul']");
-    private final By DRESSES = By.xpath(".//a[@class = 'img']");
-    private final By COLORS = By.xpath(".//ul[@class = 'col-lg-12 layered_filter_ul color-group']/li");
+    private final By ARTICLES = By.xpath(".//ul[contains(@class, 'menu-content')]/li/a");
+    private final By DRESSES = By.xpath(".//ul[@class = 'clearfix']/li/h5/a");
+    private final By COLORS = By.xpath(".//ul[contains(@class, 'color-group')]/li/label/a");
     private static final Logger LOGGER = LogManager.getLogger(AutomationPractice.class);
     WebDriver browser;
 
@@ -23,45 +23,47 @@ public class AutomationPractice {
     public void PageClicking() {
         LOGGER.info("Opening browser");
         System.setProperty("webdriver.chrome.driver", "D:/chromedriver.exe");
-        WebDriver browser = new ChromeDriver();
+        browser = new ChromeDriver();
         browser.manage().window().maximize();
         browser.get(HOME_PAGE);
 
         LOGGER.info("Opening list of Elements and clicking button");
-        tabWomenByName("Women").click();
+        tabWomenByName("WOMEN").click();
 
 
         LOGGER.info("Dresses button click ");
-        subCategoryByName("Dresses").click();
+        subCategoryByName("DRESSES").click();
 
 
         LOGGER.info("Choosing color 'Orange and clicking'");
+        WebDriverWait wait = new WebDriverWait(browser, 30);
+        wait.until(ExpectedConditions.visibilityOfAllElementsLocatedBy(COLORS));
         borderOfColoursByName("Orange").click();
 
-        WebDriverWait wait = new WebDriverWait(browser, 10);
-        wait.until(ExpectedConditions.visibilityOfAllElementsLocatedBy(COLORS));
     }
 
-    private WebElement tabWomenByName(String tab) {
+    private WebElement tabWomenByName(String name) {
         List<WebElement> tabWomanCategory = browser.findElements(ARTICLES);
         for (int i = 0; i < tabWomanCategory.size(); i++) {
             WebElement element = tabWomanCategory.get(i);
-            if (element.getText().equals(tab)) {
+            if (element.getText().equals(name)) {
                 return element;
             }
         }
         return null;
     }
-    private WebElement subCategoryByName (String colour){
+
+    private WebElement subCategoryByName (String name){
         List<WebElement> subCategoryDresses = browser.findElements(DRESSES);
         for(int i = 0; i < subCategoryDresses.size(); i++){
             WebElement dresses = subCategoryDresses.get(i);
-            if (dresses.getText().equals(colour)) {
+            if (dresses.getText().equals(name)) {
                 return dresses;
             }
         }
         return null;
     }
+
     private WebElement borderOfColoursByName (String name) {
         List<WebElement> borderOfColours = browser.findElements(COLORS);
         for(int i = 0; i < borderOfColours.size(); i++){
@@ -72,8 +74,9 @@ public class AutomationPractice {
         }
         return null;
     }
-    @AfterEach
-    public void browserClose() {
-        browser.close();
-    }
+
+//    @AfterEach
+//    public void browserClose() {
+//        browser.close();
+//    }
 }
